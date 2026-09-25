4fffbc144214aa0362b7cc9ac7659afa1fbf38e0
Dorababu Karri <dorababukarri0209@gmail.com>
2026-09-25 08:36:38 +0530

    Time: 2 ms (99.7%), Space: 45.6 MB (75.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -8,17 +8,16 @@ class Solution {
         int freq[]=new int[128];
         for(char ch:t.toCharArray()) freq[ch]++;
         int st=-1;
-        int minLength=n1;
+        int minLength=n1+1;
         for(int right=0;right<n1;right++){
             char ch=s.charAt(right);
             if(freq[ch]>0) count--;
-            // System.out.println(count);
             freq[ch]--;
             if(count==0){
                 while(count==0){
                     int len=right-left+1;
                     ch=s.charAt(left);
-                    if(len<=minLength){
+                    if(len<minLength){
                         st=left;
                         minLength=len;
                     }
