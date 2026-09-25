fdca3a1c598f6da5fd0bbbcfac38cd192237cdbf
Dorababu Karri <dorababukarri0209@gmail.com>
2026-09-25 14:58:57 +0530

    Time: 85 ms (11.78%), Space: 223.6 MB (77.03%) - LeetHub

--- 4057-number-of-intersecting-interval-pairs-ii/4057-number-of-intersecting-interval-pairs-ii.java
+++ 4057-number-of-intersecting-interval-pairs-ii/4057-number-of-intersecting-interval-pairs-ii.java
@@ -1,22 +1,21 @@
 class Solution {
     public long countIntersectingIntervals(int[][] intervals) {
         long count=0;
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         int n=intervals.length;
         int arr1[]=new int[n];
         int arr2[]=new int[n];
         int j=0;
         for(int i[]:intervals) arr1[j++]=i[0];
-        Arrays.sort(arr1);
         j=0;
         for(int i[]:intervals) arr2[j++]=i[1];
         
         for(int i=0;i<n;i++){
             int idx=Ceil(arr1,arr2[i]);
-            count+=(n-idx);
+            count+=(idx-1-i);
             
         }
-        long total=(long)n*(n-1)/2;
-        return total-count;
+        return count;
     }
     public int Ceil(int a[],int k){
         int low=0,high=a.length-1;
